2515e7841e82ed9426b27d5d663500a0488f5ace
Bartosz Kostrzewa <bartosz_kostrzewa@fastmail.com>
2025-03-04 17:10:54 +0100

    add results for tiling scan

---
+++ tiling_scan/nvidia_a100/tiling_scan.sh
@@ -0,0 +1,40 @@
+# approximately similarly sized 4D, 3D and 2D array extents
+n4=(  4  8  12  16  24   32   48   64   80   96   112   128   144   160   192 )
+n3=(  8 16  32  40  64  128  180  256  360  440   540   640   760   860  1100 ) 
+n2=( 16 64 144 256 576 1024 2304 4096 6400 9216 12544 16384 20736 25600 36864 )
+
+echo nt n factor policy kernel bw > results.dat 
+
+# 2 sockets, 64 cores per socket, 32 cores "per gpu"
+for nt in 32; do
+
+  export OMP_NUM_THREADS=$nt
+  export OMP_PLACES=cores
+  export OMP_PROC_BIND=close
+  
+  for n in $( seq 0 $(( ${#n4[@]} - 1 )) ); do
+    for f in 1 2 4 8 16 32 64; do
+      N=$(( ${n2[$n]}*${n2[$n]} ))
+      results=( $(./stream-kokkos-2d-mdrange-tiling-scan -n ${n2[$n]} -f ${f} | grep GB | awk '{print $1 " " $2}') )
+      for i in $(seq 0 4); do
+        echo $nt $N $f 2d-mdrange ${results[$(( 2*$i ))]} ${results[$(( 2*$i + 1 ))]} | tee -a results.dat
+      done
+
+      N=$(( ${n3[$n]}*${n3[$n]}*${n3[$n]} ))
+      results=( $(./stream-kokkos-3d-mdrange-tiling-scan -n ${n3[$n]} -f ${f} | grep GB | awk '{print $1 " " $2}') )
+      for i in $(seq 0 4); do
+        echo $nt $N $f 3d-mdrange ${results[$(( 2*$i ))]} ${results[$(( 2*$i + 1 ))]} | tee -a results.dat
+      done
+      
+      # on GPUs we exceed the maximum number of threads per block if the factor is larger
+      # than 32
+      if [ ${f} -le 32 ]; then
+        N=$(( ${n4[$n]}*${n4[$n]}*${n4[$n]}*${n4[$n]} ))
+        results=( $(./stream-kokkos-4d-mdrange-tiling-scan -n ${n4[$n]} -f ${f} | grep GB | awk '{print $1 " " $2}') )
+        for i in $(seq 0 4); do
+          echo $nt $N $f 4d-mdrange ${results[$(( 2*$i ))]} ${results[$(( 2*$i + 1 ))]} | tee -a results.dat
+        done
+      fi
+    done
+  done || break
+done
---
+++ tiling_scan/plot_results.R
@@ -0,0 +1,84 @@
+require(hadron)
+require(dplyr)
+require(ggplot2)
+
+archs <- c(#"dual_epyc3_7713_gcc",
+           #"dual_epyc3_7713_clang",
+           "dual_epyc2_7742_gcc",
+           "dual_xeon_8468_gcc", 
+           #"dual_xeon_8468_clang", 
+           "nvidia_a100", 
+           "amd_mi250")
+
+bws <- data.frame(bw=c(#204.8, 2*204.8,
+                       #204.8, 2*204.8,
+                       2*204.8,
+                       2*307.2,
+                       #307.2, 2*307.2,
+                       1555,
+                       1638),
+                  ylim=c(#600, 600,
+                         #600, 600,
+                         600, #600,
+                         900, #900,
+                         #900, 900,
+                         2000,
+                         1800),
+                  architecture = c(#"dual_epyc3_7713_gcc", "dual_epyc3_7713_gcc",
+                                   #"dual_epyc3_7713_clang", "dual_epyc3_7713_clang",
+                                   "dual_epyc2_7742_gcc", #"dual_epyc2_7742_gcc",
+                                   "dual_xeon_8468_gcc", #"dual_xeon_8468_gcc",
+                                   #"dual_xeon_8468_clang", "dual_xeon_8468_clang",
+                                   "nvidia_a100", 
+                                   "amd_mi250"),
+                  nt=c(#64, 128,
+                       #64, 128,
+                       128,
+                       96, 
+                       #48, 96, 
+                       32, 
+                       7))
+
+tikzfiles <- hadron::tikz.init("kokkos_stream_mdrange_tiling_scan", width=14, height=6)
+
+for( i in 1:length(archs) ){
+  arch <- archs[i]
+  bwdat <- dplyr::filter(bws, architecture == arch)
+  dat <- read.table(sprintf("%s/results.dat", arch), header=TRUE)
+  ncol <- length(unique(dat$factor))
+  maxdat <- dplyr::group_by(dat, factor, policy, nt) %>%
+            dplyr::filter(n == max(n)) %>%
+            dplyr::filter(bw == max(bw)) %>%
+            dplyr::ungroup()
+
+  p <- ggplot2::ggplot(dat, aes(x = n*8*10^(-6), y = bw, colour = kernel, shape = kernel)) +
+       ggplot2::geom_line() +
+       ggplot2::geom_point() +
+       ggplot2::geom_hline(data = bwdat, aes(yintercept = bw), colour = "blue") +
+       ggplot2::geom_hline(data = maxdat, aes(yintercept = bw), colour = "red") +
+       ggplot2::geom_point(data = bwdat, shape = NA, fill = NA, colour = NA, x = 1,
+                           aes(y = bw)) +
+       ggplot2::geom_label(data = bwdat, 
+                           aes(label = sprintf("mem %0.0f GB/s", bw),
+                               y = bw, x = 0.03),
+                           colour = "blue",
+                           inherit.aes=FALSE, 
+                           size = 2.5) +
+       ggplot2::geom_label(data = maxdat, 
+                           aes(label = sprintf("%0.0f GB/s", bw),
+                               y = bw, x = 0.03),
+                           colour = "red",
+                           inherit.aes=FALSE, 
+                           size = 2.5) +
+       ggplot2::ggtitle(hadron::escapeLatexSpecials(sprintf("%s, OMP_PROC_BIND=close, OMP_PLACES=cores", arch))) +
+       ggplot2::facet_wrap(sprintf("policy = %s", policy) ~ sprintf("tiling factor = %02d", factor), ncol = ncol) +
+       ggplot2::scale_x_continuous(trans = "log10") +
+       ggplot2::coord_cartesian(ylim = c(0, bwdat$ylim[1])) +
+       ggplot2::labs(x = "vector size [MB]",
+                     y = "BW [GB/s]") +
+       ggplot2::theme_bw() +
+       ggplot2::theme(plot.title = element_text(color="blue", size = 10, face="bold")) 
+  plot(p)
+}
+
+hadron::tikz.finalize(tikzfiles, crop=FALSE)
